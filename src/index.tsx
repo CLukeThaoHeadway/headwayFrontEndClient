3c24c25bca76bee6b4b61c834e223523fb1b79b0
CLukeThaoHeadway <102250392+CLukeThaoHeadway@users.noreply.github.com>
2022-03-23 13:36:15 -0500

    start

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { render } from "react-dom";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
